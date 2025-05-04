d511693d2d0f52b279b911f639626468e7c222c2
mbudis23 <muhammadbudisetiawan@mail.ugm.ac.id>
2025-05-04 19:41:06 +0700

    feat: NavbarOption Component

---
+++ src/components/molecules/NavbarOption.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function NavbarOption() {
+  return (
+    <div className="flex gap-[24px]">
+      <Link href={"/"} className="text-[16px] font-medium text-[#1E293B]">
+        Home
+      </Link>
+      <Link href={"/"} className="text-[16px] font-medium text-[#1E293B]">
+        About
+      </Link>
+      <Link href={"/"} className="text-[16px] font-medium text-[#1E293B]">
+        Contact
+      </Link>
+    </div>
+  );
+}
