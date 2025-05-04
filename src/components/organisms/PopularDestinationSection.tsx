31ff9ad320ea4b2fedd9ad3e4e4a91b883b2440d
mbudis23 <muhammadbudisetiawan@mail.ugm.ac.id>
2025-05-04 23:37:39 +0700

    feat: PopularDestination Component

---
+++ src/components/organisms/PopularDestinationSection.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import H2 from "../atoms/H2";
+import H3 from "../atoms/H3";
+import P from "../atoms/P";
+import CardWithImage1 from "../molecules/CardWithImage1";
+
+const DUMMY_DATA = [
+  {
+    title: "Tegallang Rice Terrace",
+    location: "Bali",
+    rating: "4.8",
+    description:
+      "Experience the breathtaking beauty of Tegallang Rice Terrace, a UNESCO World Heritage site, where lush green rice paddies cascade down the hillsides, creating a stunning landscape that showcases Bali's agricultural heritage.",
+  },
+  {
+    title: "Tegallang Rice Terrace",
+    location: "Bali",
+    rating: "5.0",
+    description:
+      "Experience the breathtaking beauty of Tegallang Rice Terrace, a UNESCO World Heritage site, where lush green rice paddies cascade down the hillsides, creating a stunning landscape that showcases Bali's agricultural heritage.",
+    src: "/img/bgImage-heroSection-1.png",
+  },
+];
+
+export default function PopularDestinationSection() {
+  return (
+    <div className="w-full bg-white p-[80px] flex flex-col gap-[24px]">
+      <H2 className="text-[#48847F] text-center">Popular Destinations</H2>
+      <P className="text-gray-400 text-center">
+        Discover the most loved destinations across Indonesia's beautiful
+        archipelago
+      </P>
+      <div className="flex p-[24px] mx-[-80px] gap-[16px]">
+        {DUMMY_DATA.map((item, index) => {
+          return (
+            <div key={index}>
+              <CardWithImage1
+                alt={item.title}
+                description={item.description}
+                location={item.location}
+                rating={item.rating}
+                src={item.src}
+                title={item.title}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
