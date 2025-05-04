4af4086b5b4f2de685f05fb002848d7d2316589b
mbudis23 <muhammadbudisetiawan@mail.ugm.ac.id>
2025-05-04 19:40:51 +0700

    feat: DisplayLogoName component

---
+++ src/components/molecules/DisplayLogoName.tsx
@@ -0,0 +1,9 @@
+import H3 from "../atoms/H3";
+
+export default function DisplayLogoName() {
+  return (
+    <H3 bold="font-bold" className="text-[#0D9488]">
+      Wisata<span className="text-[#F59E0B]">Gas</span>
+    </H3>
+  );
+}
