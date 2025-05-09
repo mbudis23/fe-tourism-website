6826b994a6bf05f5fe375f4bd7ad4567e2a74db7
mbudis23 <muhammadbudisetiawan@mail.ugm.ac.id>
2025-05-09 22:51:34 +0700

    update: hero section text logic button

--- src/components/molecules/HeroSectionText.tsx
+++ src/components/molecules/HeroSectionText.tsx
@@ -1,8 +1,15 @@
+"use client";
 import Button from "../atoms/Button";
 import H1 from "../atoms/H1";
 import P from "../atoms/P";
 
 export default function HeroSectionText() {
+  const startExploring = () => {
+    const target = document.getElementById("search");
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
     <div className="flex flex-col items-start gap-[24px] w-full">
       <H1 className="text-[#F59E0B]">
@@ -12,7 +19,9 @@ export default function HeroSectionText() {
         Explore breathtaking landscapes, vibrant cultures, agriculture, and
         unforgettable experiences across the Indonesian archipelago.
       </P>
-      <Button className="bg-[#F59E0B] text-white">Start Exploring</Button>
+      <Button onClick={startExploring} className="bg-[#F59E0B] text-white">
+        Start Exploring
+      </Button>
     </div>
   );
 }
